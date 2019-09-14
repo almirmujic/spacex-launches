a401e45fd3a761b2a2165816c872df549de2206c
Almir <contact.almir@gmail.com>
2019-09-14 17:42:00 -0500

    Rename Modal component to Launch

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import {Switch, Route} from 'react-router-dom'
 import App from './App'
-import Modal from './components/Modal'
+import Launch from './components/Launch'
 
 export default class Routes extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path="/" component={App} />
-                <Route path="/:id" component={Modal} />                
+                <Route path="/:id" component={Launch} />                
             </Switch>
         )
     }

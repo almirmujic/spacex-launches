f01747c0dd6398c5e56a515efba219e1f3ff104c
Almir <contact.almir@gmail.com>
2019-09-14 17:33:32 -0500

    Pull API data onto main page and add pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
+import { Link } from 'react-router-dom';
+
+//components
+import Thumbnail from './components/Thumbnail';
+
+//styling
 import './App.css';
 
+// https://api.spacexdata.com/v3/launches/
+
 function App() {
+  const [launches, setLaunches] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [offsetNum, setOffsetNum] = useState(0);
+
+  const pullData = (data) => {
+      const goodData = data.map(function(launch){
+        return {
+          missionName: launch.mission_name,
+          smallPatch: launch.links.mission_patch_small,
+          flightNum: launch.flight_number,
+          year: launch.launch_year
+        }
+      });
+      return goodData;
+  }
+
+  const goBack = () => {
+    setOffsetNum(offsetNum - 20);
+  }
+
+  const goNext = () => {
+    setOffsetNum(offsetNum + 20);
+  }
+
+  useEffect(()=>{
+    async function fetchLaunches(){
+      setLoading(true);
+      const res = await fetch(`https://api.spacexdata.com/v3/launches/?limit=20${offsetNum > 0 ? `&offset=${offsetNum}` : '' }`);
+      const data = await res.json();
+      console.log(data)
+      setLaunches(pullData(data));
+      setLoading(false);
+    }
+
+    fetchLaunches();
+  },[offsetNum])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header h1">Welcome to SpaceX launches</h1>
+      <div className='launch-grid'>
+        {
+          loading ? <h1>Loading...</h1> : 
+          launches.map(launch=><Link to={`/${launch.flightNum}`} className="thumbnail-link"><Thumbnail missionName={launch.missionName} smallPatch={launch.smallPatch} year={launch.year} /></Link>)         
+        }
+      </div>
+      <div>
+        <button onClick={goBack}>Back</button>
+        <button onClick={goNext}>Next</button>      
+      </div>
     </div>
   );
 }

e6dfeca5aba775f6825f09ab62847d824de81d92
Almir <contact.almir@gmail.com>
2019-09-18 16:15:21 -0500

    Convert inline styling to Styled Components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,28 @@ import styled from 'styled-components';
 import { LeftArrow } from 'styled-icons/boxicons-solid/LeftArrow';
 import { RightArrow } from 'styled-icons/boxicons-solid/RightArrow';
 
+const Input = styled.input`
+    display: block;
+    margin: 0 auto;
+    font-size: 16px;
+    letter-spacing: .2px;
+    margin-bottom: 3em;
+    padding: .7em 1.3em; 
+    border-radius: 24px;
+    border: 1px solid #dfe1e5;
+`
+
+const ArrowContainer = styled.div`
+  display: flex; 
+  justify-content: center; 
+  padding: 2em; 
+`
+
+const Arrow = styled.div`
+  width: 2em;
+  height: 2em;
+`
+
 const StyledLeftArrow = styled(LeftArrow) `
   opacity: .2;
   &:hover{
@@ -27,8 +49,6 @@ const StyledRightArrow = styled(RightArrow) `
     cursor: pointer;
   }
 `
-
-// https://api.spacexdata.com/v3/launches/
 
 function App() {
   const initialOffset = JSON.parse(window.localStorage.getItem('offset') || '0');
@@ -63,20 +83,19 @@ function App() {
   useEffect(() => {
     async function fetchLaunches() {
       setLoading(true);
+      let api = '';
       if (filter === '') {
-        const res = await fetch(`https://api.spacexdata.com/v3/launches/?limit=24${offsetNum > 0 ? `&offset=${offsetNum}` : ''}`);
-        const data = await res.json();
-        const newData = pullData(data);
-        setLaunches(newData);
+        api = `https://api.spacexdata.com/v3/launches/?limit=24${offsetNum > 0 ? `&offset=${offsetNum}` : ''}`
+        window.localStorage.setItem('offset', JSON.stringify(offsetNum));
         showPagination(true);
       } else {
         showPagination(false);
-        const res = await fetch(`https://api.spacexdata.com/v3/launches/`);
-        const data = await res.json();
-        const newData = pullData(data).filter(launch => launch.missionName.toLowerCase().includes(filter.toLowerCase()));
-        setLaunches(newData);
+        api = `https://api.spacexdata.com/v3/launches/`
       }
-      window.localStorage.setItem('offset', JSON.stringify(offsetNum));
+      const res = await fetch(api);
+      const data = await res.json();
+      const newData = pullData(data);
+      setLaunches(newData);
       setLoading(false);
     }
 
@@ -84,27 +103,36 @@ function App() {
   }, [offsetNum, filter])
 
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setFilter(e.target.value);
   }
+
+  const filterLaunches = launches.filter(launch => launch.missionName.toLowerCase().includes(filter.toLowerCase()));
+
+  const mapLaunches = filterLaunches.map(launch =>
+      <Link to={`/${launch.flightNum}`} key={launch.flightNum} className="thumbnail-link">
+          <Thumbnail missionName={launch.missionName} smallPatch={launch.smallPatch} year={launch.year} />
+      </Link>
+  );
+
+  const arrows = 
+    <ArrowContainer>
+      { offsetNum < 24 ? '' : <Arrow onClick={goBack}><StyledLeftArrow /></Arrow> }
+      { launches.length < 24 ? '' : <Arrow onClick={goNext}><StyledRightArrow /></Arrow> }
+    </ArrowContainer>;
+
 
   return (
     <div className="App">
       <h1 className="header h1">Welcome to SpaceX launches</h1>
-      <input type='text' onChange={handleChange} />
+      <Input type='text' placeholder='Search mission...' onChange={handleChange} />
       {
-        loading ? <h1>Loading...</h1> :
+        loading ? <h1 style={{ textAlign: 'center' }}>Loading...</h1> :
           <>
             <div className='launch-grid'>
-              {launches.map(launch => <Link to={`/${launch.flightNum}`} key={launch.flightNum} className="thumbnail-link"><Thumbnail missionName={launch.missionName} smallPatch={launch.smallPatch} year={launch.year} /></Link>)}
+              { mapLaunches }
             </div>
-            {
-              pagination ?
-                <div style={{ display: 'flex', justifyContent: 'center', padding: '2em' }}>
-                  {offsetNum < 24 ? '' : <div style={{ width: '2em', height: '2em' }} onClick={goBack}><StyledLeftArrow /></div>}
-                  {launches.length < 24 ? '' : <div style={{ width: '2em', height: '2em' }} onClick={goNext}><StyledRightArrow /></div>}
-                </div> : ''
-            }
+              { pagination ? arrows : '' }
           </>
       }
     </div>

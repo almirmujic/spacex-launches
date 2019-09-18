6e14cdcd0796a5c39235cc400a5509e9f856f3dd
Almir <contact.almir@gmail.com>
2019-09-18 16:32:44 -0500

    Create AnchorLink Styled Component to replace inline anchor tag styling

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,6 +6,15 @@ import { Wikipedia } from 'styled-icons/boxicons-logos/Wikipedia';
 import { Reddit } from 'styled-icons/boxicons-logos/Reddit';
 import { News } from 'styled-icons/boxicons-solid/News';
 
+const AnchorLink = styled.a`
+     display: flex;
+     flex-direction: column; 
+     margin: 1em;
+     color: inherit; 
+     opacity: .4;
+     text-decoration: none;
+`
+
 export default function Links(props) {
 
     const links = [
@@ -27,20 +36,17 @@ export default function Links(props) {
     ]
 
     return (
-        <div>
-            <h3>Links</h3>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                {
-                    links.filter(link => link.link !== null).map(extraLink =>
-                        <div key={extraLink.name}>
-                            <a href={extraLink.link} rel="noopener noreferrer" target="_blank" style={{ display: 'flex', flexDirection: 'column', margin: '1em', color: 'inherit', opacity: '.4', textDecoration: 'none' }}>
-                                <extraLink.icon style={{ height: '50px' }} />
-                                <span>{extraLink.name}</span>
-                            </a>
-                        </div>
-                    )
-                }
-            </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {
+                links.filter(link => link.link !== null).map(extraLink =>
+                    <div key={extraLink.name}>
+                        <AnchorLink href={extraLink.link} rel="noopener noreferrer" target="_blank">
+                            <extraLink.icon style={{ height: '40px' }} />
+                            <span style={{ fontSize: '14px' }}>{extraLink.name}</span>
+                        </AnchorLink>
+                    </div>
+                )
+            }
         </div>
     )
 }

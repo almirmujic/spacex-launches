ac6469ff6fc28631e2f8d7a9263b876e3b16947f
Almir <contact.almir@gmail.com>
2019-09-17 22:53:17 -0500

    Formatting

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import App from './App'
 import Launch from './components/Launch'
 
@@ -8,7 +8,7 @@ export default class Routes extends Component {
         return (
             <Switch>
                 <Route exact path="/" component={App} />
-                <Route path="/:id" component={Launch} />                
+                <Route path="/:id" component={Launch} />
             </Switch>
         )
     }

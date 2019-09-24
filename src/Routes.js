8565895e78091bc4af7c0ab703e9ff70b1008b73
Almir <contact.almir@gmail.com>
2019-09-23 23:49:59 -0500

    Wrap project in provider

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { DataProvider } from './context/DataContext';
 import App from './App'
 import Launch from './components/Launch'
 
 export default class Routes extends Component {
     render() {
         return (
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/:id" component={Launch} />
-            </Switch>
+            <DataProvider>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/:id" component={Launch} />
+                </Switch>
+            </DataProvider>
         )
     }
 }

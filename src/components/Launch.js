8d0ac89d5a49cbe3f60ab38a31a9b2c800429536
Almir <contact.almir@gmail.com>
2019-09-23 23:55:43 -0500

    Add loading conditional if fetching data - arrange links before video

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+
+//context
+import { DataContext } from '../context/DataContext';
 
 //components
 import Media from './Media';
@@ -14,30 +17,32 @@ const Patch = styled.img`
     margin: 1em; 
 `
 
+
 function Launch({ match }) {
+
+    // const [setFilter] = useContext(DataContext);
     const [info, setInfo] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const formatData = (data) => {
-        return {
-            details: data.details,
-            name: data.rocket.rocket_name,
-            location: data.launch_site.site_name,
-            video: data.links.video_link,
-            youtube: data.links.youtube_id,
-            imgs: data.links.flickr_images,
-            smallPatch: data.links.mission_patch_small,
-            patch: data.links.mission_patch,
-            reddit: data.links.reddit_launch,
-            article: data.links.article_link,
-            wiki: data.links.wikipedia,
-            missionName: data.mission_name,
-            flightNum: data.flight_number,
-            year: data.launch_year
-        }
-    }
-
     useEffect(() => {
+        const formatData = (data) => {
+            return {
+                details: data.details,
+                name: data.rocket.rocket_name,
+                location: data.launch_site.site_name,
+                video: data.links.video_link,
+                youtube: data.links.youtube_id,
+                imgs: data.links.flickr_images,
+                smallPatch: data.links.mission_patch_small,
+                patch: data.links.mission_patch,
+                reddit: data.links.reddit_launch,
+                article: data.links.article_link,
+                wiki: data.links.wikipedia,
+                missionName: data.mission_name,
+                flightNum: data.flight_number,
+                year: data.launch_year
+            }
+        }
         async function fetchLaunch() {
             setLoading(true);
             const res = await fetch(`https://api.spacexdata.com/v3/launches/${match.params.id}`);
@@ -46,24 +51,26 @@ function Launch({ match }) {
             setInfo(objData);
             setLoading(false)
         }
-
         fetchLaunch();
     }, [match.params.id])
 
-
-
     return (
         <div className="App" >
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-                <Patch src={info.patch} style={{ maxWidth: '200px', margin: '1em' }} />
-                <h1>{info.missionName}</h1>
-                <h3 style={{ margin: '.2em', opacity: '.4' }}>{info.name}</h3>
-                <span style={{ opacity: '.4' }}>{info.year}</span>
-                <h2 style={{ margin: '1em' }}>{info.location}</h2>
-                <p style={{ lineHeight: '1.5em', letterSpacing: '.6px' }}>{info.details}</p>
-            </div>
-            <Media info={info} />
-            <Links info={info} />
+            {
+                loading ? <h1 style={{ textAlign: 'center', padding: '3em' }}>Loading...</h1> :
+                    <>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                            <Patch src={info.patch} style={{ maxWidth: '200px', margin: '1em' }} />
+                            <h1>{info.missionName}</h1>
+                            <h3 style={{ margin: '.2em', opacity: '.4' }}>{info.name}</h3>
+                            <span style={{ opacity: '.4' }}>{info.year}</span>
+                            <h2 style={{ margin: '1em' }}>{info.location}</h2>
+                            <p style={{ lineHeight: '1.5em', letterSpacing: '.6px' }}>{info.details}</p>
+                        </div>
+                        <Links info={info} />
+                        <Media info={info} />
+                    </>
+            }
         </div >
     )
 }

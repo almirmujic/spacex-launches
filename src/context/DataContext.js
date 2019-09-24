055fab826a5aff9329942d3703d548f8411af232
Almir <contact.almir@gmail.com>
2019-09-23 23:58:06 -0500

    Pull in fetching of data, state, and data formatting into Context api

---
+++ src/context/DataContext.js
@@ -0,0 +1,53 @@
+import React, { createContext, useState, useEffect } from 'react'
+
+export const DataContext = createContext();
+
+export function DataProvider(props) {
+    const initialOffset = JSON.parse(window.localStorage.getItem('offset') || '0');
+
+    const [launches, setLaunches] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [offsetNum, setOffsetNum] = useState(initialOffset);
+    const [filter, setFilter] = useState('');
+    const [pagination, showPagination] = useState(true);
+
+    const pullData = (data) => {
+        const goodData = data.map(function (launch) {
+            return {
+                missionName: launch.mission_name,
+                smallPatch: launch.links.mission_patch_small,
+                flightNum: launch.flight_number,
+                year: launch.launch_year
+            }
+        });
+        return goodData;
+    }
+
+    useEffect(() => {
+        async function fetchLaunches() {
+            setLoading(true);
+            let api = '';
+            if (filter === '') {
+                api = `https://api.spacexdata.com/v3/launches/?limit=24${offsetNum > 0 ? `&offset=${offsetNum}` : ''}`
+                window.localStorage.setItem('offset', JSON.stringify(offsetNum));
+                showPagination(true);
+            } else {
+                showPagination(false);
+                api = `https://api.spacexdata.com/v3/launches/`
+            }
+            const res = await fetch(api);
+            const data = await res.json();
+            const newData = pullData(data);
+            setLaunches(newData);
+            setLoading(false);
+        }
+        fetchLaunches();
+    }, [offsetNum, filter])
+
+
+    return (
+        <DataContext.Provider value={[launches, loading, offsetNum, setOffsetNum, pagination, filter, setFilter]}>
+            {props.children}
+        </DataContext.Provider>
+    )
+}

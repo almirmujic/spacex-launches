78bd1666dc1d8be727528d2d9ef9becd9eedbad8
Almir <contact.almir@gmail.com>
2019-09-24 16:12:15 -0500

    Add not available image if patch not provided by API

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -5,7 +5,7 @@ export default function Thumbnail(props) {
         <div>
             {props.smallPatch ?
                 <img alt={props.missionName} src={props.smallPatch} style={{ width: '70%' }}></img>
-                : ''
+                : <img src='https://community.coreldraw.com/resized-image/__size/680x1000/__key/CommunityServer.Discussions.Components.Files/704/7418.NO.png' alt='Not Available' style={{ width: '70%' }}></img>
             }
             <div style={{ paddingTop: '.5em', letterSpacing: '.3px' }}>
                 <span>{props.missionName}</span>

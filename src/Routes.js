5241d920b35aadcd750c97396cc7aabf8cc2da88
Almir <contact.almir@gmail.com>
2019-10-02 18:34:31 -0500

    Add semicolons to imports

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 import { ThemeProvider } from './context/ThemeContext';
 import Footer from './components/Footer';
-import App from './App'
-import Launch from './components/Launch'
+import App from './App';
+import Launch from './components/Launch';
 
 export default class Routes extends Component {
     render() {
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
 
 //styling
 import styled from 'styled-components';

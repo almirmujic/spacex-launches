b08d5f7fa7b9590423d86b7a7a31f31617a00b21
Almir <contact.almir@gmail.com>
2019-10-06 22:42:46 -0500

    Render video only if props provides the data needed to embed it

--- src/components/Media.js
+++ src/components/Media.js
@@ -2,37 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Media(props) {
+
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', margin: '2em 0', paddingBottom: '1em', width: '100%' }}>
-            {
-                props.info.youtube === null ? '' :
-                    <div style={{
-                        position: 'relative',
-                        overflow: 'hidden',
-                        paddingTop: '56.25%',
-                    }}>
-                        <iframe
-                            style={{
-                                position: 'absolute',
-                                top: 0,
-                                left: 0,
-                                width: '100%',
-                                height: '100%',
-                                border: 0
-                            }}
-                            id='iframe'
-                            src={`https://www.youtube.com/embed/${props.info.youtube}`}
-                            title={props.info.missionName}
-                            frameBorder="0"
-                            allow='autoplay; encrypted-media'
-                            allowFullScreen
-                        />
-                    </div>
-            }
-        </div>
+        props.info.video ?
+            <div style={{ display: 'flex', flexDirection: 'column', margin: '2em 0', paddingBottom: '1em', width: '100%' }}>
+                <div style={{
+                    position: 'relative',
+                    overflow: 'hidden',
+                    paddingTop: '56.25%',
+                }}>
+                    <iframe
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                            border: 0
+                        }}
+                        id='iframe'
+                        src={`https://www.youtube.com/embed/${props.info.youtube}`}
+                        title={props.info.missionName}
+                        frameBorder="0"
+                        allow='autoplay; encrypted-media'
+                        allowFullScreen
+                    />
+                </div>
+            </div>
+            :
+            ''
     )
 }
 
 Media.propTypes = {
-    info: PropTypes.object.isRequired
+    info: PropTypes.object
 }

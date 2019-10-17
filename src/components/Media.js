c46f2ff1c1511957380e1af1bfe5a7fe63c7da2c
Almir <contact.almir@gmail.com>
2019-10-16 23:14:13 -0500

    Clean inline jsx styling

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+//styling
+import styled from 'styled-components';
+
+const VideoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 2em 0;
+    padding-bottom: 1em;
+    width: 100%;
+`
+
+const VideoWrapper = styled.div`
+    position: relative;
+    overflow: hidden;
+    padding-top: 56.25%;
+`
+
+const VideoIframe = styled.iframe`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+`
+
 export default function Media(props) {
 
     return (
         props.info.video ?
-            <div style={{ display: 'flex', flexDirection: 'column', margin: '2em 0', paddingBottom: '1em', width: '100%' }}>
-                <div style={{
-                    position: 'relative',
-                    overflow: 'hidden',
-                    paddingTop: '56.25%',
-                }}>
-                    <iframe
-                        style={{
-                            position: 'absolute',
-                            top: 0,
-                            left: 0,
-                            width: '100%',
-                            height: '100%',
-                            border: 0
-                        }}
+            <VideoContainer>
+                <VideoWrapper>
+                    <VideoIframe
                         id='iframe'
                         src={`https://www.youtube.com/embed/${props.info.youtube}`}
                         title={props.info.missionName}
@@ -27,8 +41,8 @@ export default function Media(props) {
                         allow='autoplay; encrypted-media'
                         allowFullScreen
                     />
-                </div>
-            </div>
+                </VideoWrapper>
+            </VideoContainer>
             :
             ''
     )

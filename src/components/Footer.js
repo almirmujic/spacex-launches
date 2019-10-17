c245d857c59e4b3a23fdb9c45f8c055fbf97786e
Almir <contact.almir@gmail.com>
2019-10-16 23:51:13 -0500

    Clean footer styling by creating styled component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+//styling
+import styled from 'styled-components';
+
+const FooterElement = styled.h4`
+    margin: 0 auto;
+    width: 80%;
+    max-width: 980px;
+    position: relative;
+    opacity: .6;
+    text-align: center;
+    padding: 2em 1em 2em 1em;
+    & a {
+        color: inherit;
+    }
+`
+
 export default function Footer() {
     return (
-        <h4 className='App footer'>Created by <a href='https://almir.dev'>Almir</a></h4>
+        <FooterElement>Created by <a href='https://almir.dev'>Almir</a></FooterElement>
     )
 }
